b88b67e8e20780898cb33c50632b3bf05d693db8
opilane <opilane@HP-CZC2349HTS>
2023-05-02 11:00:19 +0300

    Add project files.

---
+++ Praktiline töö Uss/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Uss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '-';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            
+
+
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y,char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+
+}
