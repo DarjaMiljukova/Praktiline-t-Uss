8b03bcd8b2aab34af509077ac469d02d06c091d6
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-15 12:34:34 +0300

    lesson 6
    .

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Uss2;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');
+           
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            
+            p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            Console.ReadLine();
+        }
+
+        //static void Draw(int x, int y, char sym)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.Write(sym);
+        
+        //Music_mäng = new music();
+        //ConsoleKeyInfo nupp = neew ConsoleKeyInfo();
+        //_ = mäng.Tagaplaanis_Mangida("");
+    }
+}
