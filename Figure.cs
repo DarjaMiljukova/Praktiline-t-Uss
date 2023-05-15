d0580c903c43b6c6480035a8327feacbe1d88c91
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-15 13:51:43 +0300

    lesson 8
    .

---
+++ Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uss2
+{
+    internal class Figure
+    {
+        protected List<Point> pList;
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Uss2;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
+
+            HorizontalLine Upline = new HorizontalLine(0, 78, 0, '*');
+            HorizontalLine Downline = new HorizontalLine(0, 78, 24, '*');
+            VerticalLine Leftline = new VerticalLine(0, 24, 0, '*');
+            VerticalLine Rightline = new VerticalLine(0, 24, 78, '*');
+            Upline.Drow();
+            Downline.Drow();
+            Leftline.Drow();
+            Rightline.Drow();
+
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+        }
+    }
+}
+//static void Draw(int x, int y, char sym)
+//{
+//    Console.SetCursorPosition(x, y);
+//    Console.Write(sym);
+
+//Music_mäng = new music();
+//ConsoleKeyInfo nupp = neew ConsoleKeyInfo();
+//_ = mäng.Tagaplaanis_Mangida("");
+
