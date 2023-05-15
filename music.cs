5b4bf14dba171a06f59b1817e7a3075c4e12cccc
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-15 12:39:19 +0300

    lesson 7
    .

---
+++ music.cs
@@ -0,0 +1,49 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uss2
+{
+    public class music
+    {
+        public async Task Tagaplaanis_Mangida(string Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audioFileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent { DesiredLatency = 200 })
+                {
+                    waveOutDevice.Init(audioFileReader);
+                    waveOutDevice.Play();
+                    while (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+
+
+        public async Task Natuke_Mangida(string Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audioFileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent())
+                {
+                    waveOutDevice.Init(audioFileReader);
+                    waveOutDevice.Play();
+                    while (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(50);
+                    }
+                }
+            });
+        }
+
+    }
+}
+
