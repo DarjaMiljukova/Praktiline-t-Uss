4988588d376d2fe360fc2d3483a740568ef0a4ae
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-26 09:48:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Uss2;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VerticalLine vl = new VerticalLine(0, 10, 5, '%');
+            Draw(vl);
+
+            Point p = new Point(4, 5, '*');
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
+
+            HorizontalLine hl = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(vl);
+            figures.Add(hl);
+
+            foreach (var f in figures)
+            {
+                f.Draw();
+            }
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
+    }
+}
+//static void Draw(int x, int y, char sym)
+//{
+//    Console.SetCursorPosition(x, y);
+//    Console.Write(sym);
+
+//Music_mäng = new music();
+//ConsoleKeyInfo nupp = neew ConsoleKeyInfo();
+//_ = mäng.Tagaplaanis_Mangida("");
+
