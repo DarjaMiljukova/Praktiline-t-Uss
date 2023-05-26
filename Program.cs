2d1811c14f524a8ba80e9040321a192524116ded
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-26 09:35:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Uss2;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+        Direction direction;
+
+        public Snake(Point tail, int length, Direction _direction)
+        {
+            direction = _direction;
+            pList = new List<Point>();
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
+        }
+
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point(head);
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
+    }
+}
+//static void Draw(int x, int y, char sym)
+//{
+//    Console.SetCursorPosition(x, y);
+//    Console.Write(sym);
+
+//Music_mäng = new music();
+//ConsoleKeyInfo nupp = neew ConsoleKeyInfo();
+//_ = mäng.Tagaplaanis_Mangida("");
+
