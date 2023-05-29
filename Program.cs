1c8509814b0475df51aafeb52857872183f3aee1
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-05-29 14:46:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -14,17 +14,17 @@ namespace Uss2
     {
         static void Main(string[] args)
         {
-            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
             Walls walls = new Walls(80, 25);
             walls.Draw();
 
             // Отрисовка точек			
-            Point p = new Point(4, 5, '*');
+            Point p = new Point(4, 5, '█');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            FoodCreator foodCreator = new FoodCreator(80, 25, '❋');
             Point food = foodCreator.CreateFood();
             food.Draw();
 
