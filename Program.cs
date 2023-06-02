15f4f61a6ad3016f04f7ac57576e6155afd4a709
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-06-02 10:48:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Uss2;
+
+namespace Uss2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25);
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '█');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '▲');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+    
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("GAME OVER", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
+        }
+
+    }
+}
+//static void Draw(int x, int y, char sym)
+//{
+//    Console.SetCursorPosition(x, y);
+//    Console.Write(sym);
+
+//Music_mäng = new music();
+//ConsoleKeyInfo nupp = neew ConsoleKeyInfo();
+//_ = mäng.Tagaplaanis_Mangida("");
